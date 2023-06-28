05c2f1e065bbca06030e96d49af27d14809b0585
王志浩 <1269928993@qq.com>
2023-06-28 20:59:18 +0800

    chore：init project

---
+++ packages/vue2-child/src/layout/Layout.vue
@@ -0,0 +1,11 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+export default {
+  name: 'Layout'
+};
+</script>
+
+<style scoped lang="less"></style>
---
+++ packages/vue2-child/src/views/CoexistView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>vue2 CoexistView</div>
+</template>
+
+<script>
+export default {
+  name: 'CoexistView'
+};
+</script>
+
+<style scoped lang="less"></style>
