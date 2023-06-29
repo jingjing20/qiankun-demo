99fa6de4d27bcc551e74a7caddb1df7c2813127f
jingjing20 <jingjing20@users.noreply.github.com>
2023-06-29 05:49:34 +0000

    deploy: f923a5c417963d24b343e27c58952789f95325a7

---
+++ vue2-child/js/789.69097e93.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackJsonp_@qiankun-demo/vue2-child"]=self["webpackJsonp_@qiankun-demo/vue2-child"]||[]).push([[789],{29789:function(e,n,u){u.r(n),u.d(n,{default:function(){return c}});var i=function(){var e=this,n=e._self._c;return n("div",[e._v("vue2 CoexistView")])},s=[],t={name:"CoexistView"},l=t,o=u(64334),a=(0,o.Z)(l,i,s,!1,null,"4ae11712",null),c=a.exports}}]);
+//# sourceMappingURL=789.69097e93.js.map
