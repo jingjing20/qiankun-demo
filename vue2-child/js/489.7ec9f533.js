f642209abbd9d60259a5b12b5925145d1274650e
jingjing20 <jingjing20@users.noreply.github.com>
2023-06-29 04:42:14 +0000

    chore：update deploy

--- vue2-child/js/489.7ec9f533.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackJsonp_@qiankun-demo/vue2-child"]=self["webpackJsonp_@qiankun-demo/vue2-child"]||[]).push([[489],{35489:function(e,n,u){u.r(n),u.d(n,{default:function(){return s}});var t=function(){var e=this,n=e._self._c;return n("router-view")},r=[],a={name:"Layout"},i=a,l=u(64334),o=(0,l.Z)(i,t,r,!1,null,"f6650a90",null),s=o.exports}}]);
-//# sourceMappingURL=489.7ec9f533.js.map
